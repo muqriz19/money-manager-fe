fc1740f27e32a8596453ef59eeec2816b2a6277a
muqriz19 <muqriz19othman@gmail.com>
2024-07-26 16:08:56 +0800

    fix wording

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="page">
 	<h1>Money Manager</h1>
-	<p>Manage and control your income/expenses</p>
+	<p>Manage and control your income & expenses</p>
 	<hr />
 	<p>Begin to do so here...</p>
 	<a href="/login" class="d-block">Login here</a>
